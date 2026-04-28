db03573d09e348a281af420b44eb5f2307941fb4
seriguchi_keigo <2364138keigo@gmail.com>
2026-04-28 10:11:34 +0900

    Supabaseクライアントの環境変数を修正

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -3,6 +3,6 @@ import type { Database } from "../../database.types";
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient<Database>(
-  import.meta.env.VITE_REACT_APP_SUPABASE_URL,
-  import.meta.env.VITE_REACT_APP_SUPABASE_ANON_KEY,
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
 );
