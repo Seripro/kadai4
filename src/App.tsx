f1f8cdac13b5ee2e23996d7f12ed5e25f2166f85
seriguchi_keigo <2364138keigo@gmail.com>
2026-04-28 16:44:30 +0900

    名刺新規登録ページへのルートを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { UserDetail } from "./pages/UserDetail";
+import { Register } from "./pages/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/cards/:id" element={<UserDetail />} />
+        <Route path="/cards/register" element={<Register />} />
       </Routes>
     </Router>
   );
