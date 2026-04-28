a9249003b4ddd4d86bf16a991871aa0516f2d1df
seriguchi_keigo <2364138keigo@gmail.com>
2026-04-28 10:11:46 +0900

    ユーザー詳細表示機能を実装し、ユーザー情報を取得するためのAPI呼び出しを追加

--- src/pages/UserDetail.tsx
+++ src/pages/UserDetail.tsx
@@ -1,6 +1,43 @@
+import type { User } from "@/types/userTypes";
+import { getUserById } from "@/utils/supabaseFunctions";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export const UserDetail = () => {
+  const [user, setUser] = useState<User>();
+  const [loading, setLoading] = useState<boolean>(false);
   const { id } = useParams();
-  return <div>{id}</div>;
+
+  useEffect(() => {
+    if (!id) return;
+
+    const getUser = async () => {
+      setLoading(true);
+      try {
+        const res = await getUserById(id);
+        console.log(res);
+        setUser(res.data?.[0]);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+
+    getUser();
+  }, [id]);
+
+  if (!id) return <div>not found</div>;
+  if (loading) return <div>loading...</div>;
+  if (!user) return <div>not found</div>;
+
+  return (
+    <>
+      <div>名前：{user.name}</div>
+      <div>自己紹介：{user.description}</div>
+      <div>スキル：</div>
+      <div>GitHub：{user.github_id}</div>
+      <div>Qiita：{user.qiita_id}</div>
+      <div>X：{user.x_id}</div>
+    </>
+  );
 };
