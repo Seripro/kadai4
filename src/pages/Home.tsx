465c5b0f216efc72f569c662319a176fd82696e9
seriguchi_keigo <2364138keigo@gmail.com>
2026-04-29 19:03:30 +0900

    入力したIDが存在しないときのバリデーションチェックを追加

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,46 @@
-import { Button, Input } from "@chakra-ui/react";
-import { useState } from "react";
+import { getAllUsers } from "@/utils/supabaseFunctions";
+import { Button, Input, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
-  const [id, setId] = useState<string>();
+  const [id, setId] = useState<string>("");
+  const [ids, setIds] = useState<string[]>([]);
+  const [error, setError] = useState<string>();
   const navigate = useNavigate();
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setId(e.target.value);
   };
   const handleClick = () => {
-    navigate(`/cards/${id}`);
+    setError("");
+    if (ids.includes(id)) {
+      navigate(`/cards/${id}`);
+    } else {
+      setError("そのIDは存在しません");
+    }
+    if (id == "") setError("IDを入力して下さい");
   };
+  useEffect(() => {
+    const getAllId = async () => {
+      try {
+        const users = await getAllUsers();
+        const newIds = users.map((user) => user.user_id);
+        setIds(newIds);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getAllId();
+  }, []);
   return (
     <>
       <h1>デジタル名刺アプリ</h1>
       <Input onChange={handleChange} />
+      {error && (
+        <Text color="red.500" fontSize="sm" style={{ textAlign: "left" }}>
+          {error}
+        </Text>
+      )}
       <Button onClick={handleClick}>表示する</Button>
     </>
   );
