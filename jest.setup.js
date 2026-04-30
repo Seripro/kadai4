723477b9d45e4bbacfd8b61d84756b8a281348c7
seriguchi_keigo <2364138keigo@gmail.com>
2026-04-30 13:34:16 +0900

    なんでこんなに設定がいるの？これがないとテストが通らない

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,18 @@
 require("@testing-library/jest-dom");
 require("dotenv").config();
+global.TextEncoder = require("util").TextEncoder;
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    };
+  };
+if (!global.structuredClone) {
+  global.structuredClone = function structuredClone(objectToClone) {
+    if (objectToClone === undefined) return undefined;
+    return JSON.parse(JSON.stringify(objectToClone));
+  };
+}
